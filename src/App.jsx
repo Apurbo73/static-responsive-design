1c4a12a8fbb821ecc2245236e41b9ab6d7e17226
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-05 03:02:20 +0600

    carousel design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,132 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <div>
-    hi
-  </div>
-  )
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div className="container-fluid">
+          <h3 className="navbar-brand " href="#">
+            <span className="text-dark">Design Service</span>
+          </h3>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#Home">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#About"
+                >
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#Services"
+                >
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#Portfolio"
+                >
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={0}
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={1}
+            aria-label="Slide 2"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={2}
+            aria-label="Slide 3"
+          />
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="image/home-1.jpg" className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block text-light">
+              <h5>Your Dream House Design</h5>
+              <button className="btn btn-warning">CLick For More</button>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="image/home-1.jpg" className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block text-light">
+              <h5>Your Dream House Design</h5>
+              <button className="btn btn-warning">CLick For More</button>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src="image/home-1.jpg" className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block text-light">
+              <h5>Your Dream House Design</h5>
+              <button className="btn btn-warning">CLick For More</button>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
